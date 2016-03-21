c7dbcdf43c3444001fd3711e8b5b5d5481622889
Valeri Karpov <valkar207@gmail.com>
2016-03-21 16:15:45 -0700

    initial

---
+++ src/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const _ = require('lodash');
+
+class Schema {
+  constructor(obj) {
+    this._obj = _.cloneDeep(obj);
+    this._paths = {};
+  }
+
+  compile() {
+    this._paths = visitor(this._obj);
+  }
+
+  path(path, props) {
+    if (!props) {
+      return _.get(this._obj, path);
+    }
+    _.set(this._obj, path, props);
+    return this;
+  }
+
+  paths() {
+    return _.map(Object.keys(this._paths),
+      path => { key: path, value: this[path] });
+  }
+}
+
+function visitor(obj) {
+  var paths = paths || {};
+
+  visitObject(obj, '', paths);
+  return paths;
+}
+
+function visitArray(arr, path, paths) {
+  paths[path] = { $type: Array };
+  if (arr.length > 0) {
+    if (Array.isArray(arr[0])) {
+      visitArray(arr[0], path + '.$', paths);
+    } else if (typeof arr[0] === 'object') {
+      visitObject(arr[0], path + '.$', paths);
+    } else {
+      paths[path + '.$'] = { $type: arr[0] };
+    }
+  } else {
+    paths[path + '.$'] = { $type: Object }
+  }
+  return paths[path];
+}
+
+function visitObject(obj, path, paths) {
+  let keys = Object.keys(obj);
+  if (keys.length > 0 && keys[0].charAt(0) === '$') {
+    paths[path] = obj;
+    return;
+  }
+
+  if (path) {
+    paths[path] = { $type: Object, $schema: obj };
+  }
+  _.each(obj, function(value, key) {
+    if (Array.isArray(value)) {
+      visitArray(value, join(path, key), paths);
+    } else if (typeof value === 'object') {
+      visitObject(value, join(path, key), paths);
+    } else {
+      paths[join(path, key)] = { $type: value };
+    }
+  });
+}
+
+function join(path, key) {
+  if (path) {
+    return path + '.' + key;
+  }
+  return key;
+}
+
+module.exports = Schema;
---
+++ test/unit.test.js
@@ -0,0 +1,85 @@
+'use strict';
+
+let assert = require('assert');
+let Schema = require('../src');
+
+describe('schema', function() {
+  it('compiles paths', function() {
+    let schema = new Schema({
+      test: Number,
+      nested: {
+        a: {
+          $type: Number
+        }
+      }
+    });
+
+    schema.compile();
+
+    assert.deepEqual(schema._paths, {
+      test: { $type: Number },
+      nested: { $type: Object, $schema: { a: { $type: Number } } },
+      'nested.a': { $type: Number }
+    });
+  });
+
+  it('handles arrays', function() {
+    let schema = new Schema({
+      test: Number,
+      arrMixed: [],
+      arrPlain: [Number],
+      arrNested: [[Number]]
+    });
+
+    schema.compile();
+
+    assert.deepEqual(schema._paths, {
+      'test': { $type: Number },
+      'arrMixed': { $type: Array },
+      'arrMixed.$': { $type: Object },
+      'arrPlain': { $type: Array },
+      'arrPlain.$': { $type: Number },
+      'arrNested': { $type: Array },
+      'arrNested.$': { $type: Array },
+      'arrNested.$.$': { $type: Number }
+    });
+  });
+
+  it('handles nested document arrays', function() {
+    let schema = new Schema({
+      docs: [{ _id: Number }]
+    });
+
+    schema.compile();
+
+    assert.deepEqual(schema._paths, {
+      'docs': { $type: Array },
+      'docs.$': { $type: Object, $schema: { _id: Number } },
+      'docs.$._id': { $type: Number }
+    });
+  });
+
+  it('treats keys that start with $ as a terminus', function() {
+    let schema = new Schema({
+      test: {
+        $prop: 1
+      }
+    });
+
+    schema.compile();
+
+    assert.deepEqual(schema._paths, {
+      'test': { $prop: 1 }
+    });
+  });
+
+  it('adding paths with .path()', function() {
+    let schema = new Schema({
+      docs: [{ _id: Number }]
+    });
+
+    assert.ok(!schema.path('_id'));
+    schema.path('_id', { $type: Number });
+    assert.deepEqual(schema.path('_id'), { $type: Number });
+  });
+});
