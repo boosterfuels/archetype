1ac1555385ff0b314e2ec45c8065dfeeb0754788
Valeri Karpov <valkar207@gmail.com>
2016-11-02 19:42:00 -0700

    fix(defaults): make defaults recursive

--- src/defaults.js
+++ src/defaults.js
@@ -3,33 +3,21 @@
 module.exports = applyDefaults;
 
 const _ = require('lodash');
+const join = require('./unmarshal/util').join;
 const mpath = require('mpath');
+const realPathToSchemaPath = require('./unmarshal/util').realPathToSchemaPath;
 const shouldSkipPath = require('./util').shouldSkipPath;
 
 function applyDefaults(obj, schema, projection) {
-  _.each(Object.keys(schema._paths), path => {
-    if (!('$default' in schema._paths[path])) {
-      return;
-    }
-    if (shouldSkipPath(projection, path) || projection.$noDefaults) {
-      return;
-    }
-    const _path = path.replace(/\.\$\./g, '.').replace(/\.\$$/g, '');
-    const val = mpath.get(_path, obj);
-    if (Array.isArray(val)) {
-      for (let i = 0; i < val.length; ++i) {
-        if (val[i] == null) {
-          val[i] = handleDefault(schema._paths[path].$default, obj);
-        }
-      }
-      mpath.set(_path, val, obj);
-    } else if (val == null) {
-      mpath.set(_path, handleDefault(schema._paths[path].$default, obj), obj);
+  _.each(Object.keys(schema._obj), key => {
+    const def = defaults(obj, obj[key], schema, key, projection)
+    if (def !== void 0 && obj[key] == null) {
+      obj[key] = def;
     }
   });
 }
 
-function defaults(v, key, schema, path, projection) {
+function defaults(root, v, schema, path, projection) {
   if (shouldSkipPath(projection, path) || projection.$noDefaults) {
     return;
   }
@@ -37,13 +25,29 @@ function defaults(v, key, schema, path, projection) {
   const fakePath = realPathToSchemaPath(path);
   const schemaPath = schema._paths[fakePath];
 
-  if (schemaPath) {
-    if (schemaPath.$type === Object && schemaPath.$schema) {
-      _.each(schemaPath.$schema, (value, key) => check(root, _.get(v, key), schema, join(fakePath, key), error, projection));
-    }
-    if (schemaPath.$type === Array) {
-      _.each(v || [], (value, index) => check(root, value, schema, join(fakePath, index.toString()), error, projection));
-    }
+  if (!schemaPath) {
+    return;
+  }
+
+  if ('$default' in schemaPath) {
+    return handleDefault(schemaPath.$default, root);
+  }
+
+  if (schemaPath.$type === Object && schemaPath.$schema) {
+    _.each(schemaPath.$schema, (value, key) => {
+      const def = defaults(root, _.get(v, key), schema, join(fakePath, key), projection);
+      if (def !== void 0 && value == null) {
+        v[key] = def;
+      }
+    });
+  }
+  if (schemaPath.$type === Array) {
+    _.each(v || [], (value, index) => {
+      const def = defaults(root, value, schema, join(fakePath, index.toString()), projection);
+      if (def !== void 0 && value == null) {
+        v[index] = def;
+      }
+    });
   }
 }
 
