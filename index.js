7f5e1e8a8deb472a6eaed25f31ff838a57f92651
Valeri Karpov <valkar207@gmail.com>
2017-09-21 10:21:25 -0700

    feat(helpers): add matchType

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports = require('./src').Archetype;
-module.exports.to = require('./src/to');
+
 module.exports.CastError = require('./src').CastError;
+module.exports.matchType = require('./src/helpers/matchType');
+module.exports.to = require('./src/to');
---
+++ test/helpers.test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const assert = require('assert');
+const { matchType } = require('../');
+
+describe('matchType', function() {
+  it('works with JSON.parse()', function() {
+    const parse = matchType({ string: JSON.parse });
+
+    const obj = { hello: 'world' };
+
+    // If given a string, will parse it
+    assert.deepEqual(parse(JSON.stringify(obj)), obj);
+
+    // If not, will do nothing
+    assert.strictEqual(parse(obj), obj);
+    assert.strictEqual(parse(null), null);
+    assert.strictEqual(parse(undefined), undefined);
+  });
+
+  it('works with trimming strings', function() {
+    const trim = matchType({ string: str => str.trim() });
+
+    // If given a string, will trim it
+    assert.equal(trim('  abc '), 'abc');
+    assert.equal(trim('abc'), 'abc');
+
+    // If not, will do nothing
+    const obj = {};
+    assert.strictEqual(trim(obj), obj);
+    assert.strictEqual(trim(null), null);
+    assert.strictEqual(trim(undefined), undefined);
+  });
+});
